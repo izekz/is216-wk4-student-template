f49165eb7b8ecf5264a0e84235381162227109bd
izekz <crazycewl@gmail.com>
2026-09-06 16:34:17 +0800

    Exercise 1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,6 +3,8 @@
 
     // Add code here
     // challenge: use array  
+    let message = "Choose your favourite fruit:"
+    let [fruit1, fruit2, fruit3] = ['apple', 'orange', 'peach']
     
 </script>
 
